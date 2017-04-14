bc7921ddd4de447f5a0ee629a78685984a7a5aaa
John Wood <j@jdtw.us>
2017-04-13 22:40:11 -0700

    Move from nbits to nbytes

--- cl-cbor.lisp
+++ cl-cbor.lisp
@@ -23,30 +23,33 @@ type (the high-order 3 bits) and additional information (the low-order 5 bits)."
    (ldb (byte 3 5) b)
    (ldb (byte 5 0) b)))
 
-(defun len-information (len &aux (bits (integer-length len)))
+(defun len-information (len)
+  "The 5-bit additional information is either the integer itself (for additional
+information values 0 through 23) or the length of additional data. Additional
+information 24 means the value is represented in an additional uint8_t, 25 means
+a uint16_t, 26 means a uint32_t, and 27 means a uint64_t."
   (declare ((unsigned-byte 64) len))
-  (cond
-    ((<= len 23) len)
-    ((<= bits 8) 24)
-    ((<= bits 16) 25)
-    ((<= bits 32) 26)
-    ((<= bits 64) 27)))
+  (let ((bits (integer-length len)))
+    (cond
+      ((<= len 23) len)
+      ((<= bits 8) 24)
+      ((<= bits 16) 25)
+      ((<= bits 32) 26)
+      ((< bits 64) 27))))
 
-(defun int-padding (nbits)
+(defun int-padding (nbytes)
   (let ((pad (cond
-               ((<= nbits 16) 0)
-               ((<= nbits 32) (- 4 (ceiling nbits 8)))
-               ((<= nbits 64) (- 8 (ceiling nbits 8))))))
+               ((<= nbytes 2) 0)
+               ((<= nbytes 4) (- 4 nbytes))
+               ((<= nbytes 8) (- 8 nbytes)))))
     (if (> pad 0)
         (loop repeat pad collect 0))))
 
 (defun int->bytes (n)
   (declare ((unsigned-byte 64) n))
-  (let* (bytes
-         (nbits (integer-length n))
-         (nbytes (ceiling nbits 8)))
+  (let ((nbytes (ceiling (integer-length n) 8)) bytes)
     (dotimes (i nbytes)
       (locally
           (declare (optimize (safety 0)))
         (push (ldb (byte 8 (* i 8)) n) bytes)))
-    (nconc (int-padding nbits) bytes)))
+    (nconc (int-padding nbytes) bytes)))
